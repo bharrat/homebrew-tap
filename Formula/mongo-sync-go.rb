fa92958f6b44a2670ad6d842b5977d3793e5d25a
goreleaserbot <goreleaser@carlosbecker.com>
2018-04-27 20:12:53 -0700

    mongo-sync-go version v0.1.0

--- Formula/mongo-sync-go.rb
+++ Formula/mongo-sync-go.rb
@@ -3,7 +3,7 @@ class MongoSyncGo < Formula
   homepage "https://github.com/bharrat/mongo-sync-go/blob/master/README.md"
   url "https://github.com/bharrat/mongo-sync-go/releases/download/v0.1.0/mongo-sync-go_0.1.0_darwin_amd64.tar.gz"
   version "0.1.0"
-  sha256 "a8313ca65d0746433ff87654cbfec99bb61a6a66aa22cea14713d8d8d74346c8"
+  sha256 "0423a486ed2cdf08300fc51371cf20cb260868e565f058c46dd652fdcdc350a9"
 
   def install
     bin.install "mongo-sync-go"
